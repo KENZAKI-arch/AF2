d28e408b863f7facc440991ea118992a162c07c9
KENZAKI-arch <yourgithubemail@gmail.com>
2026-05-22 13:57:38 +0800

    auto update

--- autopush.bat
+++ autopush.bat
@@ -1,7 +1,7 @@
 @echo off
 :loop
 timeout /t 10 /nobreak >nul
-cd "C:\Users\luigi\Documents\GitHub\AF2"
+cd "C:\Users\luigi\Downloads\AutofishSYSTEMGIT\AF2"
 git add .
 git commit -m "auto update" >nul 2>&1
 git push >nul 2>&1
